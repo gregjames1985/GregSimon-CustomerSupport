92a57db310728931bad8983d5448629399f8a655
gsimon85 <137004812+gsimon85@users.noreply.github.com>
2023-07-12 22:19:17 -0500

    Updated part of assignment 4

---
+++ src/main/java/com/example/gregsimoncustomersupport/Attachment.java
@@ -0,0 +1,23 @@
+package com.example.gregsimoncustomersupport;
+
+public class Attachment {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getContents() {
+        return contents;
+    }
+
+    public void setContents(byte contents) {
+        this.contents = contents;
+    }
+
+    private String name;
+
+    private byte contents;
+}
---
+++ src/main/java/com/example/gregsimoncustomersupport/Ticket.java
@@ -0,0 +1,77 @@
+package com.example.gregsimoncustomersupport;
+
+import java.util.HashMap;
+
+public class Ticket {
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public String getBodyOfTicket() {
+        return bodyOfTicket;
+    }
+
+    public void setBodyOfTicket(String bodyOfTicket) {
+        this.bodyOfTicket = bodyOfTicket;
+    }
+
+    public HashMap<String, String> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(HashMap<String, String> attachments) {
+        this.attachments = attachments;
+    }
+
+    String customerName;
+    String Subject;
+    String bodyOfTicket;
+    HashMap<String,String> attachments = new HashMap<>();
+
+    public Ticket(String customerName, String Subject, String bodyOfTicket, HashMap<String,String> attachments){
+        this.customerName = customerName;
+        this.Subject = Subject;
+        this.bodyOfTicket = bodyOfTicket;
+        this.attachments = attachments;
+    }
+    public void Tickets(){
+
+    }
+    public void addAttactment(String attachmentTitle, String attachmentBody){
+        //adds attachment
+        this.attachments.put(attachmentTitle, attachmentBody);
+    }
+    public int getNumberOfAttachments(){
+        //Get the Number of attachments
+        return this.attachments.size();//get Individual attachment
+    }
+    public String getIndividualAttachment(int indexValue){
+        //get Individual attachment
+        // get the key set
+        Set<Integer> keySet = this.attachments.keySet();
+        Integer[] keyArray = keySet.toArray(new Integer[keySet.size()]);
+        // taking input of index
+        Integer index = indexValue;
+        Integer key = keyArray[index - 1];
+        return this.attachments.get(indexValue);
+    }
+    public HashMap<String, String> getAllAttachments(){
+        //get All attachment
+       return this.attachments;
+    }
+
+
+
+}
